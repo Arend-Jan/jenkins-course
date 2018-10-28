35097f502b2620bd1dec10d0b4c9407d15881257
Arend-Jan <github@filmplaza.nl>
2018-10-28 22:30:42 +0100

    add ajoosterveld as root of dockerhub

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -5,7 +5,7 @@ job('NodeJS Docker example') {
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
-    triggers {
+    triggers {arend
         scm('H/5 * * * *')
     }
     wrappers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ajoosterveld/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
